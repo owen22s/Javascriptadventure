8b1a439312b1345a2b87d00ea6d2c2217ae0ca3d
Owen Stas <34884@ma-web.nl>
2022-12-07 15:18:27 +0100

    soorteer machine

--- web/les2-boodschappen/script.js
+++ web/les2-boodschappen/script.js
@@ -1,1 +1,23 @@
 console.log("script is geladen")
+
+const groentevak = document.getElementById('groente');
+const fruitvak = document.getElementById('fruit');
+
+function zetinfruitla(fruit){
+    console.log(fruit);
+
+
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = fruit;
+    fruitvak.appendChild(nieuwElement);
+
+}
+function zetingroentela(groente){
+    console.log(groente);
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = groente;
+    groentevak.appendChild(nieuwElement);
+
+}
